b1805a35f57526b2ed50ece4f17b2f1f67e238de
william-monroy <william_monroy777@hotmail.com>
2021-08-28 22:53:58 -0500

    sidebar

--- src/Pages/dashboard/Dashboard.jsx
+++ src/Pages/dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ import './Dashboard.css'
 
 const Dashboard = () => {
     return (
-        <div>
+        <div className="Dashboard">
             {/* Navbar */}
             <Navbar />
             {/* Sidebar */}
