5c0392cef6f402b94f04c748f995405840285b62
william-monroy <william_monroy777@hotmail.com>
2021-08-28 22:03:50 -0500

    fix map items

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -5,45 +5,51 @@ import MenuItem from '../menuItem/MenuItem';
 
 const Sidebar = () => {
 
-    const sidebar = {
-        Inicio: {
+    let sidebar = [
+        {
             icon: <HomeFilled />,
             text: 'Inicio',
             color: '#FF7551',
             enable: true,
         },
-        Cursos: {
+        {
             icon: <BookFilled />,
             text: 'Cursos',
             color: '#22B07D',
             enable: false,
         },
-        Entretenimiento: {
+        {
             icon: <RocketFilled />,
             text: '#FFC700',
             color: '',
             enable: false,
         },
-        Estadisticas: {
+        {
             icon: <PieChartFilled />,
             text: 'Estadísticas',
             color: '#E890FF',
             enable: false,
         },
-        Ajustes: {
+        {
             icon: <SettingFilled />,
             text: 'Ajustes',
             color: '#839DFE',
             enable: false,
         },
-    }
+    ]
 
     return (
         <div className="Sidebar">
             <p className="Sidebar-title">MENU</p>
-            {sidebar.map(item => {
-                <MenuItem icon={item.icon} text={item.text} color={item.color} enabled={item.enable} />
-            })}
+            {sidebar.map((item, i) => (
+                <MenuItem
+                    key={i}
+                    icon={item.icon}
+                    text={item.text}
+                    color={item.color}
+                    enabled={item.enable}
+                />
+            ))}
         </div>
     )
 }
